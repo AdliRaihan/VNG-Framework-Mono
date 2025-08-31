78ffde907c54cfd5da0628315582a3351d2d740e
Adli <adliraihan001@gmail.com>
2025-08-31 17:49:45 +0700

    First Clean Push

---
+++ VNG/VNG.Core/Scenes/DependencyInjections/ContentInjections.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VNG.Core.Scenes.DependencyInjections
+{
+    public interface ContentInjections
+    {
+        public ContentManager getContentManager { get; }
+        public static ContentInjections instance;
+
+        public static ContentInjections GetInstance()
+        {
+            return instance;
+        }
+
+        public static void SetInstance(ContentInjections contentManager)
+        {
+            instance = contentManager;
+        }
+    }
+}
---
+++ VNG/VNG.Core/Scenes/Integrations/VNGMouse.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using VNG.WindowsDX.Scenes.DependencyInjections;
+
+namespace VNG.WindowsDX.Scenes.Helper
+{
+    internal class VNGMouse: MouseInjections
+    {
+        Vector2 mousePosition
+        {
+            get
+            {
+                return Mouse.GetState().Position.ToVector2();
+            }
+        }
+
+        bool mouseLeftUp
+        {
+            get
+            {
+                return Mouse.GetState().LeftButton == ButtonState.Released;
+            }
+        }
+
+        bool mouseRightUp
+        {
+            get
+            {
+                return Mouse.GetState().RightButton == ButtonState.Released;
+            }
+        }
+
+        bool mouseLeftDown
+        {
+            get
+            {
+                return Mouse.GetState().LeftButton == ButtonState.Pressed;
+            }
+        }
+
+        public bool mouseRightDown
+        {
+            get
+            {
+                return Mouse.GetState().LeftButton == ButtonState.Pressed;
+            }
+        }
+
+        Vector2 MouseInjections.mousePosition => mousePosition;
+
+        bool MouseInjections.mouseLeftUp => mouseLeftUp;
+
+        bool MouseInjections.mouseRightUp => mouseRightUp;
+
+        bool MouseInjections.mouseLeftDown => mouseLeftDown;
+
+        private Game game;
+
+        public VNGMouse(Game game)
+        {
+            this.game = game;
+        }
+
+        public void OnUpdate()
+        {
+            if (this.game == null)
+                return;
+
+            if (this.mouseLeftDown)
+            {
+                System.Diagnostics.Debug.WriteLine(mousePosition.ToString());
+            }
+        }
+    }
+}
