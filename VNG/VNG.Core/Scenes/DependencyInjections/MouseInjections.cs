135dbf52b71868f7e773d5574c7dc72cc245a73f
Adli <adliraihan001@gmail.com>
2025-08-31 17:49:45 +0700

    First Clean Push

---
+++ VNG/VNG.Core/Scenes/DependencyInjections/MouseInjections.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using VNG.Core.Scenes.DependencyInjections;
+
+namespace VNG.WindowsDX.Scenes.DependencyInjections
+{
+    public interface MouseInjections
+    {
+        private static MouseInjections _instance;
+        public Vector2 mousePosition { get; }
+        public bool mouseLeftUp { get; }
+        public bool mouseRightUp { get; }
+        public bool mouseLeftDown { get; }
+        public bool mouseRightDown { get; }
+        public static MouseInjections GetInstance()
+        {
+            return _instance;
+        }
+
+        public static void SetInstance(MouseInjections injector)
+        {
+            _instance = injector;
+        }
+    }
+}
